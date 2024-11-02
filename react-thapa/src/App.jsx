52037abce35014541175cef66078ec867331041d
akshay.thappar@inputzero.com <Test@123>
2024-11-02 21:58:04 +0530

    Method 3

--- react-thapa/src/App.jsx
+++ react-thapa/src/App.jsx
@@ -14,7 +14,9 @@ export const App = () => {
 };
 
 const NetflixSeries = () => {
-  const age=16;
+  const age=21;
+  let canWatch="not available";
+  if(age>18) canWatch="Watch now";
   const name = "Queen of Starts";
   const rating = 8.2;
   const summary = "Summary: Lorem ipsum, dolor sit amet consectetur adipisicing elit Ratione, nihil nemo eaque voluptatibus reprehenderit accusantium omnis! Nisi eveniet similique qui ex, minima ipsum excepturi asperiores commodisunt enim ullam quam quidem expedita assumenda porro optio temporalaudantium aliquam eos velit illo maxime! Atque debitis commodi suscipit.";
@@ -34,7 +36,7 @@ const NetflixSeries = () => {
       <h3>Rating: {5+3.2}</h3>
       <h3>Rating: {summary}</h3>
       <h3>Genre: {returnGenre()}</h3>
-      <button>{age>18?"watch now":"not available"}</button>
+      <button>{canWatch}</button>
       <p>
       
       </p>
