42e20be1f1e7b32b19452bd066ef28b1fc6d64b4
akshay.thappar@inputzero.com <Test@123>
2024-11-02 21:51:31 +0530

    Method 1

--- react-thapa/src/App.jsx
+++ react-thapa/src/App.jsx
@@ -14,12 +14,32 @@ export const App = () => {
 };
 
 const NetflixSeries = () => {
+  const age=16;
   const name = "Queen of Starts";
   const rating = 8.2;
   const summary = "Summary: Lorem ipsum, dolor sit amet consectetur adipisicing elit Ratione, nihil nemo eaque voluptatibus reprehenderit accusantium omnis! Nisi eveniet similique qui ex, minima ipsum excepturi asperiores commodisunt enim ullam quam quidem expedita assumenda porro optio temporalaudantium aliquam eos velit illo maxime! Atque debitis commodi suscipit.";
   const returnGenre = () =>{
     const genre = "RomCom";
     return genre;
+  }
+
+  if(age<18){
+    return (
+      <div>
+        <div>
+          <img src="Slide1.png" alt="img not found" width="30%" height="30%" />
+        </div>
+        <h2>Name: {name}</h2>
+        <h3>Rating: {rating}</h3>
+        <h3>Rating: {5+3.2}</h3>
+        <h3>Rating: {summary}</h3>
+        <h3>Genre: {returnGenre()}</h3>
+        <button>Not Available</button>
+        <p>
+        
+        </p>
+      </div>
+    );
   }
   return (
     <div>
@@ -31,6 +51,7 @@ const NetflixSeries = () => {
       <h3>Rating: {5+3.2}</h3>
       <h3>Rating: {summary}</h3>
       <h3>Genre: {returnGenre()}</h3>
+      <button>Watch Now</button>
       <p>
       
       </p>
