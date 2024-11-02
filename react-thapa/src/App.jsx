f70df0b94ea4a7410aae552dd38a988aa45da0da
akshay.thappar@inputzero.com <Test@123>
2024-11-02 20:25:13 +0530

    understanding fragments

--- react-thapa/src/App.jsx
+++ react-thapa/src/App.jsx
@@ -1,66 +1,69 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import React, { Fragment } from "react";
 
-// function App() {
-//   const [count, setCount] = useState(0)
+export const App = () => {
+  // return (
+  //   <div>
+  //     <NetflixSeries />
+  //     <NetflixSeries />
+  //     <NetflixSeries />
+  //     <NetflixSeries />
+  //     <NetflixSeries />
+  //   </div>
+  // );
+  // Instead of above code we can do this 
 
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
+  // Method->1 
+  // return ([<NetflixSeries key={1}/>,<NetflixSeries key={2}/>])
 
-// export default App
+  //Method->2
+  // return (<React.Fragment>
+  //    <NetflixSeries />
+  //   <NetflixSeries />
+  //   <NetflixSeries />
+  //   <NetflixSeries />
+  //   <NetflixSeries />
+  // </React.Fragment>);
 
+  // OR 
 
+  // return (<Fragment>
+  //    <NetflixSeries />
+  //   <NetflixSeries />
+  //   <NetflixSeries />
+  //   <NetflixSeries />
+  //   <NetflixSeries />
+  // </Fragment>) ; // fro this you can deireclty write import {Fragment} from 'react' this is array destructuring;
 
-import React from "react";
-// export const App = () =>{
-//   // return React.createElement("h1",null,"Hello World");
-//   return <h1>Hello World</h1>;
-// }
-
-// One method is to directly return this 
-{/* <h1>Hello world</h1> */}
-
-// or another method is to return this
-
-// React.createElement("h1",null,"Hello World");
-
-// console.log(React.createElement("h1",null,"Hello World"));
-
-
-export const App = () =>{
-  return <div>
-    <div><img src="Slide1.png" alt="img not found" width="30%" height="30%" /></div>
-    <h2>Name: Queen of Starts</h2>
-    <h3>Rating: 8.3</h3>
-    <p>Summary: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, nihil nemo eaque voluptatibus reprehenderit accusantium omnis! Nisi eveniet similique qui ex, minima ipsum excepturi asperiores commodi sunt enim ullam quam quidem expedita assumenda porro optio tempora laudantium aliquam eos velit illo maxime! Atque debitis commodi suscipit.</p>
-  </div>
-}
+  //Method->3
+  return <>
+   <NetflixSeries />
+   <NetflixSeries />
+   <NetflixSeries />
+   <NetflixSeries />
+   <NetflixSeries />
+  </>
 
 
 
 
+};
+
+const NetflixSeries = () => {
+  return (
+    <div>
+      <div>
+        <img src="Slide1.png" alt="img not found" width="30%" height="30%" />
+      </div>
+      <h2>Name: Queen of Starts</h2>
+      <h3>Rating: 8.3</h3>
+      <p>
+        Summary: Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Ratione, nihil nemo eaque voluptatibus reprehenderit accusantium omnis!
+        Nisi eveniet similique qui ex, minima ipsum excepturi asperiores commodi
+        sunt enim ullam quam quidem expedita assumenda porro optio tempora
+        laudantium aliquam eos velit illo maxime! Atque debitis commodi
+        suscipit.
+      </p>
+    </div>
+  );
+};
