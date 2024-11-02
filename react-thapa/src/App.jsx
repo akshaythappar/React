e75a4e2b2e3cf3fed20e19b34003e56f39b9200b
akshay.thappar@inputzero.com <Test@123>
2024-11-02 21:18:10 +0530

    dynamic value in jsx

--- react-thapa/src/App.jsx
+++ react-thapa/src/App.jsx
@@ -2,40 +2,7 @@
 import React, { Fragment } from "react";
 
 export const App = () => {
-  // return (
-  //   <div>
-  //     <NetflixSeries />
-  //     <NetflixSeries />
-  //     <NetflixSeries />
-  //     <NetflixSeries />
-  //     <NetflixSeries />
-  //   </div>
-  // );
-  // Instead of above code we can do this 
 
-  // Method->1 
-  // return ([<NetflixSeries key={1}/>,<NetflixSeries key={2}/>])
-
-  //Method->2
-  // return (<React.Fragment>
-  //    <NetflixSeries />
-  //   <NetflixSeries />
-  //   <NetflixSeries />
-  //   <NetflixSeries />
-  //   <NetflixSeries />
-  // </React.Fragment>);
-
-  // OR 
-
-  // return (<Fragment>
-  //    <NetflixSeries />
-  //   <NetflixSeries />
-  //   <NetflixSeries />
-  //   <NetflixSeries />
-  //   <NetflixSeries />
-  // </Fragment>) ; // fro this you can deireclty write import {Fragment} from 'react' this is array destructuring;
-
-  //Method->3
   return <>
    <NetflixSeries />
    <NetflixSeries />
@@ -44,27 +11,28 @@ export const App = () => {
    <NetflixSeries />
   </>
 
-
-
-
-
 };
 
 const NetflixSeries = () => {
+  const name = "Queen of Starts";
+  const rating = 8.2;
+  const summary = "Summary: Lorem ipsum, dolor sit amet consectetur adipisicing elit Ratione, nihil nemo eaque voluptatibus reprehenderit accusantium omnis! Nisi eveniet similique qui ex, minima ipsum excepturi asperiores commodisunt enim ullam quam quidem expedita assumenda porro optio temporalaudantium aliquam eos velit illo maxime! Atque debitis commodi suscipit.";
+  const returnGenre = () =>{
+    const genre = "RomCom";
+    return genre;
+  }
   return (
     <div>
       <div>
         <img src="Slide1.png" alt="img not found" width="30%" height="30%" />
       </div>
-      <h2>Name: Queen of Starts</h2>
-      <h3>Rating: 8.3</h3>
+      <h2>Name: {name}</h2>
+      <h3>Rating: {rating}</h3>
+      <h3>Rating: {5+3.2}</h3>
+      <h3>Rating: {summary}</h3>
+      <h3>Genre: {returnGenre()}</h3>
       <p>
-        Summary: Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-        Ratione, nihil nemo eaque voluptatibus reprehenderit accusantium omnis!
-        Nisi eveniet similique qui ex, minima ipsum excepturi asperiores commodi
-        sunt enim ullam quam quidem expedita assumenda porro optio tempora
-        laudantium aliquam eos velit illo maxime! Atque debitis commodi
-        suscipit.
+      
       </p>
     </div>
   );
