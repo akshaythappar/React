89bcb948cb61d5b473241245b5fe06091718ab44
akshay.thappar@inputzero.com <Test@123>
2024-11-03 14:12:27 +0530

    advance destructuring

--- react-thapa/src/components/SeriesCard.jsx
+++ react-thapa/src/components/SeriesCard.jsx
@@ -1,22 +1,22 @@
 export const SeriesCard = (props) =>{
     console.log(props);
-    const {currElem} = props;
+    const {id,img_url,name,rating,description,cast,genre,watch_url} = props.currElem;
         return <>
         <li>
           <div>
             <img
-              src={currElem.img_url}
-              alt={currElem.name}
+              src={img_url}
+              alt={name}
               width="30%"
               height="30%"
             />
           </div>
-          <h2>Name: {currElem.name}</h2>
-          <h3>Rating: {currElem.rating}</h3>
-          <h3>Summary: {currElem.description}</h3>
-          <h3>Genre: {currElem.genre}</h3>
-          <p>Cast: {currElem.cast}</p>
-          <a href={currElem.watch_url} target="_blank">
+          <h2>Name: {name}</h2>
+          <h3>Rating: {rating}</h3>
+          <h3>Summary: {description}</h3>
+          <h3>Genre: {genre}</h3>
+          <p>Cast: {cast}</p>
+          <a href={watch_url} target="_blank">
             <button>Watch</button>
           </a>
         </li>
