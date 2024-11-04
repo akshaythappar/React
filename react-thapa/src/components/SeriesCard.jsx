778a47ea68143c569a496b48a36f1e71cab09667
akshay.thappar@inputzero.com <Test@123>
2024-11-04 18:51:57 +0530

    implemnt conditional  styling using classname

--- react-thapa/src/components/SeriesCard.jsx
+++ react-thapa/src/components/SeriesCard.jsx
@@ -11,10 +11,11 @@ export const SeriesCard = ({ currElem }) => {
         </div>
         <div className="card-content">
           <h2>Name: {name}</h2>
-          <h3>Rating: {rating}</h3>
+          {/* <h3>Rating: <span style={{backgroundColor:rating>8.5?"green":"red"}}>{rating}</span></h3> */}
+          <h3>Rating: <span className={rating>8.5?"super-hit":"average"}>{rating}</span></h3>
           <p>Summary: {description}</p>
-          <p>Genre: {genre}</p>
-          <p>Cast: {cast}</p>
+          <p>Genre: {genre.join(",")}</p>
+          <p>Cast: {cast.join(",")}</p>
           <a href={watch_url} target="_blank">
             <button style={btn_style}>Watch</button>
           </a>
