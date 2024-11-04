a2d95283d5ecff18a4aa9c8910070ddd234e09b4
akshay.thappar@inputzero.com <Test@123>
2024-11-04 18:14:31 +0530

    adding inline styles

--- react-thapa/src/components/SeriesCard.jsx
+++ react-thapa/src/components/SeriesCard.jsx
@@ -2,7 +2,7 @@ export const SeriesCard = ({currElem}) =>{
     // console.log(props);
     const {id,img_url,name,rating,description,cast,genre,watch_url} = currElem;
         return <>
-        <li>
+        <li className="card">
           <div>
             <img
               src={img_url}
@@ -11,14 +11,16 @@ export const SeriesCard = ({currElem}) =>{
               height="30%"
             />
           </div>
+          <div className="card-content">
           <h2>Name: {name}</h2>
           <h3>Rating: {rating}</h3>
-          <h3>Summary: {description}</h3>
-          <h3>Genre: {genre}</h3>
-          <p>Cast: {cast}</p>
+          <p style={{margin:"1.2rem 0rem"}}>Summary: {description}</p>
+          <p style={{margin:"1.2rem 0rem"}}>Genre: {genre}</p>
+          <p style={{margin:"1.2rem 0rem"}}>Cast: {cast}</p>
           <a href={watch_url} target="_blank">
             <button>Watch</button>
           </a>
+          </div>
         </li>
         </>;    
 
