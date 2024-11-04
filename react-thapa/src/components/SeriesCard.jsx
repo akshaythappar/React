ad87b7daef99ec142236a0e770545aa5cef03ecb
akshay.thappar@inputzero.com <Test@123>
2024-11-04 18:24:44 +0530

    inline style using css variable

--- react-thapa/src/components/SeriesCard.jsx
+++ react-thapa/src/components/SeriesCard.jsx
@@ -1,27 +1,24 @@
-export const SeriesCard = ({currElem}) =>{
-    // console.log(props);
-    const {id,img_url,name,rating,description,cast,genre,watch_url} = currElem;
-        return <>
-        <li className="card">
-          <div>
-            <img
-              src={img_url}
-              alt={name}
-              width="30%"
-              height="30%"
-            />
-          </div>
-          <div className="card-content">
+export const SeriesCard = ({ currElem }) => {
+  // console.log(props);
+  const { id, img_url, name, rating, description, cast, genre, watch_url } =
+    currElem;
+  return (
+    <>
+      <li className="card">
+        <div>
+          <img src={img_url} alt={name} width="30%" height="30%" />
+        </div>
+        <div className="card-content">
           <h2>Name: {name}</h2>
           <h3>Rating: {rating}</h3>
-          <p style={{margin:"1.2rem 0rem"}}>Summary: {description}</p>
-          <p style={{margin:"1.2rem 0rem"}}>Genre: {genre}</p>
-          <p style={{margin:"1.2rem 0rem"}}>Cast: {cast}</p>
+          <p>Summary: {description}</p>
+          <p>Genre: {genre}</p>
+          <p>Cast: {cast}</p>
           <a href={watch_url} target="_blank">
-            <button>Watch</button>
+            <button style={{ padding: "1.2rem 2.4rem" ,border:'none',backgroundColor:"var(--btn-hover-bg-color)",color:"var(--color)"}}>Watch</button>
           </a>
-          </div>
-        </li>
-        </>;    
-
-}
+        </div>
+      </li>
+    </>
+  );
+};
