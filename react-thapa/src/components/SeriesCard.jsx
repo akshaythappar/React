9cbe088a4512b71751850a4a484cf2681c4de48e
akshay.thappar@inputzero.com <Test@123>
2024-11-05 16:45:42 +0530

    more styling

--- react-thapa/src/components/SeriesCard.jsx
+++ react-thapa/src/components/SeriesCard.jsx
@@ -2,8 +2,15 @@ export const SeriesCard = ({ currElem }) => {
   // console.log(props);
   const { id, img_url, name, rating, description, cast, genre, watch_url } =
     currElem;
-    const btn_style={ padding: "1.2rem 2.4rem" ,border:'none',backgroundColor:"var(--btn-hover-bg-color)",color:"var(--color)"};
-    const ratingClass = rating>8.5?"super-hit":"average";
+  const btn_style = {
+    padding: "1.2rem 2.4rem",
+    border: "none",
+    backgroundColor: `${rating>8.5?"#7dcea0":"#f7dc6f"}`,
+    color: "var(--color)",
+    fontWeight:"bold",
+    cursor:"pointer",
+  };
+  const ratingClass = rating > 8.5 ? "super-hit" : "average";
   return (
     <>
       <li className="card">
@@ -13,7 +20,9 @@ export const SeriesCard = ({ currElem }) => {
         <div className="card-content">
           <h2>Name: {name}</h2>
           {/* <h3>Rating: <span style={{backgroundColor:rating>8.5?"green":"red"}}>{rating}</span></h3> */}
-          <h3>Rating: <span className={`rating ${ratingClass}`}>{rating}</span></h3>
+          <h3>
+            Rating: <span className={`rating ${ratingClass}`}>{rating}</span>
+          </h3>
           <p>Summary: {description}</p>
           <p>Genre: {genre.join(",")}</p>
           <p>Cast: {cast.join(",")}</p>
