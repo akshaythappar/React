5d093b4b28462c3536801dacc5942d0a87079e38
akshay.thappar@inputzero.com <Test@123>
2024-11-11 15:09:55 +0530

    event capturing

--- react-thapa/src/components/EventPropagation.jsx
+++ react-thapa/src/components/EventPropagation.jsx
@@ -10,15 +10,15 @@ export const EventPropgation = () =>{
         console.log("Parent clicked")
     }
     const handleChildClick  = (event) =>{
-        event.stopPropagation();  // to stop the event form bublling such that when i will click on child compoentn to prevent it form calling it parent and grandparent 
+        // event.stopPropagation();  // to stop the event form bublling such that when i will click on child compoentn to prevent it form calling it parent and grandparent 
         console.log(event);
         console.log("child clicked");
     }
     return (<>
     <section className="main-div">\
-        <div className="g-parent" onClick={handleGrandParent}>
-            <div className="p-div" onClick={handleParentClick}>
-                <div className="c-div" onClick={handleChildClick}>
+        <div className="g-parent" onClickCapture={handleGrandParent}>
+            <div className="p-div" onClickCapture={handleParentClick}>
+                <div className="c-div" onClickCapture={handleChildClick}>
                     child div 
                 </div>
             </div>
