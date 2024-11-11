e6a43fca0b390d2393cbf85d14904e0f9270ab1a
akshay.thappar@inputzero.com <Test@123>
2024-11-11 17:01:23 +0530

    using useState

--- react-thapa/src/components/DerivedState.jsx
+++ react-thapa/src/components/DerivedState.jsx
@@ -1,13 +1,14 @@
+import { useState } from "react";
+
+// const users = 
 import "./EV.css";
-
-const users = [
-  {name:"Alice",age:25},
-  {name:"Bob",age:30},
-  {name:"Charlie",age:35},
-  {name:"Angles",age:45},
-]
-
 export const DerivedState = () => {
+    const [users,setUsers]=useState([
+      {name:"Alice",age:25},
+      {name:"Bob",age:30},
+      {name:"Charlie",age:35},
+      {name:"Angles",age:45},
+    ]);
   return <div className="main-div">
     <h1>Users List</h1>
     <ul>
