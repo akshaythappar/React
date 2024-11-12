fef84f094cac9970646f5ea0f2acaff48dc7876b
akshay.thappar@inputzero.com <Test@123>
2024-11-12 12:57:10 +0530

    icons in react

--- react-thapa/src/components/Projects/ToggleSwitch/ToggleSwitch.jsx
+++ react-thapa/src/components/Projects/ToggleSwitch/ToggleSwitch.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./ToggleSwitch.css";
+import { IoIosSwitch } from "react-icons/io";
 
 export const ToggleSwitch = () => {
   const [isOn, setIsOn] = useState(false);
@@ -9,7 +10,8 @@ export const ToggleSwitch = () => {
     setIsOn(!isOn);
   }
   return (
-    <>
+      <>
+      <h1>Toggle Switch : <IoIosSwitch style={{color:"red"}}/></h1>
       <div className="toggle-switch" onClick={handleToggleSwitch} style={toggleBGColor}>
         <div className={`switch ${checkIsOn}`}>
           <span className="switch-state">{checkIsOn}</span>
