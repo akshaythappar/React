cd0f85e3aa3d99976f132e7df5ffdfdf2040d4a6
akshay.thappar@inputzero.com <Test@123>
2024-11-18 15:06:15 +0530

    code done by thapa

--- react-thapa/src/App.jsx
+++ react-thapa/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { ToDoApp } from "./components/Projects/ToDoApp/ToDoApp";
+import { Todo } from "./components/Projects/ToDoApp/Todo";
 
 export const App = () => {
+  
   return (
      <>      
-      <ToDoApp/>
+      <Todo/>
     </>
   );
 };
---
+++ react-thapa/src/components/Projects/ToDoApp/Todo.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { MdCheck } from "react-icons/md";   
+import { MdDeleteForever } from "react-icons/md";
+import "./Todo.css"
+
+export const Todo = () => {
+    const [inputValue,setInputValue]=useState("");
+    const [task,setTask]=useState([]);
+    const handleInputChange = (value)=>{
+        setInputValue(value);
+    }
+    const handleFormSubmit = () =>{
+        event.preventDefault();
+        if(!inputValue) return ;
+        
+        // by defalut which you will pass in the place of previous will act as the previouse value
+        // setTask((prev)=>console.log(prev));
+        if(task.includes(inputValue))
+            {
+                setInputValue("");
+                return
+            } ;
+
+        setTask((prevTask)=>[...prevTask,inputValue])
+
+        setInputValue("");
+
+
+    }
+    return (
+        <section className="todo-container">
+            <header>
+                <h1>Todo List</h1>
+            </header>
+            <section className="form">
+                <form action="" onSubmit={handleFormSubmit}>
+                    <div>
+                        <input type="text" className="todo-input" autoComplete="off" value={inputValue} onChange={(event)=>handleInputChange(event.target.value)}/>
+                    </div>
+                    <div>
+                        <button type="submit" className="todo-btn">
+                            Add Task
+                        </button>
+                    </div>
+                </form>
+
+            </section>
+
+            <section className="myUnOrdList">
+                <ul>
+                
+                {task.map((curTask,index) => {
+                   return <li key={index} style={{color:"white"}} className="todo-item">
+                      <span> {curTask}</span>   
+                        <button className="check-btn">
+                        <button>
+                        <MdCheck />
+                        </button>
+                        </button>
+                        <button className="delete-btn">
+                        <MdDeleteForever />
+                        </button>
+                        </li>
+                    })}
+                </ul>
+            </section>
+        </section>
+    )
+}
