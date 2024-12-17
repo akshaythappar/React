cae4e34074c62ee9d73e67b6e5ab76d1259e5708
akshay.thappar@inputzero.com <Test@123>
2024-12-18 00:34:46 +0530

    understanding userstate hook

---
+++ react-thapa/src/components/hooks/useState/index.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+export const Counter = () =>{
+    const [count,setCount]=useState(0);
+    const handleIncreament = () =>{
+        setCount(count+1);
+        console.log("inner",count);
+    }   
+    console.log("outer",count);
+    return (
+        <div className="container state-container" style={{textAlign:"center"}}>
+            <h1>useState Hook!</h1>
+            <br/>
+            <p>{count}</p>
+            {/* <button className="state-button" onClick={()=>setCount(count+1)}>Increment </button> */}
+            <button className="state-button" onClick={handleIncreament}>Increment </button>
+        </div>
+    )
+}
