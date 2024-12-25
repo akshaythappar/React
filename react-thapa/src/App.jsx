d892b37753656fad7af1bf2f871c95475025a0e7
akshay.thappar@inputzero.com <Test@123>
2024-12-25 22:39:41 +0530

    Challenge implemented by thapa

--- react-thapa/src/App.jsx
+++ react-thapa/src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import {Counter} from  "./components/hooks/useState"
 import { Challenge } from "./components/hooks/useState/Challenge";
+import CounterChallenge from "./components/hooks/useState/ChallengeByThapa";
 
 
 export const App = () => {
   
   return (
      <>      
-    <Challenge/>
+    
+    <CounterChallenge/>
     </>
   );
 };
