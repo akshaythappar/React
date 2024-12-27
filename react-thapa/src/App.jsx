e6d3a448dc68197ae2a70ee372209de7fd7c2212
akshay.thappar@inputzero.com <Test@123>
2024-12-27 22:43:52 +0530

    simple way to handle form

--- react-thapa/src/App.jsx
+++ react-thapa/src/App.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import {Counter} from  "./components/hooks/useState"
 import { Challenge } from "./components/hooks/useState/Challenge";
 import CounterChallenge from "./components/hooks/useState/ChallengeByThapa";
-import { RegistrationFrom } from "./components/hooks/useState/Registration";
+import { RegistrationFromReact } from "./components/hooks/useState/ReactRegistration";
 
 
 export const App = () => {
-  
+
   return (
      <>      
     
-    <RegistrationFrom/>
+    <RegistrationFromReact/>
     </>
   );
 };
