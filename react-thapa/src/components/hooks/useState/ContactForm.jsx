794d744595082936d97751285838d93a6d44c083
akshay.thappar@inputzero.com <Test@123>
2024-12-29 16:37:51 +0530

    easy way done by me

--- react-thapa/src/components/hooks/useState/ContactForm.jsx
+++ react-thapa/src/components/hooks/useState/ContactForm.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react"
 
 export const ContactForm = ()=>{
-    const [username,setUsername]=useState("");
-    const [password,setPassword]=useState("");
-    const [message,setMessage]=useState("");
+    const [contactData,setContactData] =useState({username:"",password:"",message:"",});
 
     const handleFormSubmit = (e)=>{ 
         e.preventDefault(); 
        const formDate = {
-        username,
-        password,
-        message
+        username:contactData.username,
+        password:contactData.password,
+        message:contactData.message,
        }
        console.log(formDate);
+    }
+
+    const handleOnchange = (e) =>{
+       const {name,value}= e.target;
+       setContactData((prev)=>({...prev,[name]:value}));
     }
     return (
         <>
@@ -22,13 +25,13 @@ export const ContactForm = ()=>{
                 <h1>Contact Form</h1>
                 <form action="" onSubmit={handleFormSubmit}>
                     <label htmlFor="username">UserName</label>
-                    <input type="text" name="username" required autoComplete="off" onChange={(e)=>setUsername(e.target.value)} />
+                    <input type="text" name="username" required autoComplete="off" onChange={(e)=>handleOnchange(e)}  value={contactData.username}/>
 
                     <label htmlFor="password">UserName</label>
-                    <input type="text" name="password" required autoComplete="off" onChange={(e)=>setPassword(e.target.value)}/>
+                    <input type="text" name="password" required autoComplete="off" onChange={(e)=>handleOnchange(e)} value={contactData.password}/>
 
                     <label htmlFor="message">Message</label>
-                    <textarea name="message" type="password" required autoComplete="off" rows="6" onChange={(e)=>setMessage(e.target.value)}></textarea>
+                    <textarea name="message" type="password" required autoComplete="off" rows="6" onChange={(e)=>handleOnchange(e)} value={contactData.message}></textarea>
 
                     <button>submit</button>
                 </form>
