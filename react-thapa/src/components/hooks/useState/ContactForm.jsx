4acdd1e7118dc152a2f329b90a64862290ea541c
akshay.thappar@inputzero.com <Test@123>
2024-12-29 16:49:05 +0530

    hard way done by thapa

--- react-thapa/src/components/hooks/useState/ContactForm.jsx
+++ react-thapa/src/components/hooks/useState/ContactForm.jsx
@@ -1,22 +1,22 @@
 import { useState } from "react"
 
 export const ContactForm = ()=>{
-    const [contactData,setContactData] =useState({username:"",password:"",message:"",});
+    const [username,setUsername]=useState("");
+    const [email,setEmail]=useState("");
+    const [message,setMessage]=useState("");
 
-    const handleFormSubmit = (e)=>{ 
-        e.preventDefault(); 
-       const formDate = {
-        username:contactData.username,
-        password:contactData.password,
-        message:contactData.message,
+    const handleFormSubmit = (e)=>{
+       e.preventDefault();
+       // if the key and value are same then not need to write both the value
+       const contactDate = {
+        username,
+        email,
+        message
        }
-       console.log(formDate);
+       console.log(contactDate);
+
     }
 
-    const handleOnchange = (e) =>{
-       const {name,value}= e.target;
-       setContactData((prev)=>({...prev,[name]:value}));
-    }
     return (
         <>
         
@@ -25,13 +25,13 @@ export const ContactForm = ()=>{
                 <h1>Contact Form</h1>
                 <form action="" onSubmit={handleFormSubmit}>
                     <label htmlFor="username">UserName</label>
-                    <input type="text" name="username" required autoComplete="off" onChange={(e)=>handleOnchange(e)}  value={contactData.username}/>
+                    <input type="text" name="username" required autoComplete="off" onChange={(e)=>setUsername(e.target.value)}  value={username}/>
 
-                    <label htmlFor="password">UserName</label>
-                    <input type="text" name="password" required autoComplete="off" onChange={(e)=>handleOnchange(e)} value={contactData.password}/>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" required autoComplete="off" onChange={(e)=>setEmail(e.target.value)} value={email}/>
 
                     <label htmlFor="message">Message</label>
-                    <textarea name="message" type="password" required autoComplete="off" rows="6" onChange={(e)=>handleOnchange(e)} value={contactData.message}></textarea>
+                    <textarea name="message" type="password" required autoComplete="off" rows="6" onChange={(e)=>setMessage(e.target.value)} value={message}></textarea>
 
                     <button>submit</button>
                 </form>
