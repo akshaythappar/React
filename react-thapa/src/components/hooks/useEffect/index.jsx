9ff03453b0898c1e19cef24ed3de42626a7e91ea
akshay.thappar@inputzero.com <Test@123>
2025-01-02 11:52:49 +0530

    useEffect Example 2

--- react-thapa/src/components/hooks/useEffect/index.jsx
+++ react-thapa/src/components/hooks/useEffect/index.jsx
@@ -1,13 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./index.css";
 
 export const ReactuseEffect = () =>{
+    const [count,setCount]=useState(0);
     useEffect(()=>{
-        console.log("hello useEffect");
-    },[]);
+        console.log("Count Value:" + count);
+    },[count]);
     return (
-        <div>
-            <h1>Hello useEffect!</h1>
+        <div className="container effect-container">
+            <h1>useEffect Hook</h1>
+            <p>Count: {count}</p>
+            <button onClick={()=>setCount(count+1)}>Increment</button>
+            
         </div>
     )
 }
