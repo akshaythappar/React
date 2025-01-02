16c1f399b7288caaad3cea3c03cccc8d3b5a4a7b
akshay.thappar@inputzero.com <Test@123>
2025-01-02 12:40:42 +0530

    example 3

--- react-thapa/src/components/hooks/useEffect/index.jsx
+++ react-thapa/src/components/hooks/useEffect/index.jsx
@@ -3,14 +3,21 @@ import "./index.css";
 
 export const ReactuseEffect = () =>{
     const [count,setCount]=useState(0);
+    const [date,setDate]=useState(0);
+
+
     useEffect(()=>{
-        console.log("Count Value:" + count);
-    },[count]);
+         setInterval(() => {
+            const updatedDate = new Date();
+            setDate(updatedDate.toLocaleTimeString());
+        }, 1000);
+
+        
+    },[date]);
     return (
         <div className="container effect-container">
-            <h1>useEffect Hook</h1>
-            <p>Count: {count}</p>
-            <button onClick={()=>setCount(count+1)}>Increment</button>
+            <h1>Date : {date}</h1>
+            
             
         </div>
     )
